f96cb570975308018cb31b98eab71bb4a732e91d
Jonas Pereira <jonaspplc@hotmail.com>
2023-08-01 20:36:29 -0300

    fix adapter to deploy

--- client/src/infra/http/AxiosAdapter.ts
+++ client/src/infra/http/AxiosAdapter.ts
@@ -1,7 +1,29 @@
 import axios from "axios";
-import HttpClient from "./HttpClient";
+import HttpClient from "./httpClient";
+
 
 export default class AxiosAdapter implements HttpClient {
+
+	constructor (router: any) {
+		axios.interceptors.request.use((config: any) => {
+			const token = localStorage.getItem("token");
+			console.log('interceptor token', token);
+			
+			if (token) {
+				config.headers["Authorization"] = `Bearer ${token}`;
+			}
+			return config;
+		});
+		axios.interceptors.response.use((response) => {
+			return response;
+		}, (error) => {
+			if (error.response.status === 401) {
+				localStorage.removeItem("token");
+				router.push("/login");
+			}
+			return Promise.reject(error);
+		});
+	}
 
 	async get(url: string): Promise<any> {
 		const response = await axios({
@@ -20,4 +42,20 @@ export default class AxiosAdapter implements HttpClient {
 		return response.data;
 	}
 
+	async put(url: string, data: any): Promise<any> {
+		const response = await axios({
+			url,
+			method: "put",
+			data
+		});
+		return response.data;
+	}
+
+	async delete(url: string): Promise<any> {
+		const response = await axios({
+			url,
+			method: "delete"
+		});
+		return response.data;
+	}
 }
